70732cdb395dab73e56bb1613760180d42de45f1
Mayur Vaghasiya <mayurvaghasiya75@gmail.com>
2021-05-11 13:45:28 +0930

    updated siteinfo

--- lib/views/home.php
+++ lib/views/home.php
@@ -1,44 +1,68 @@
 <p><?php echo $message ?></p>
 
 <style media="screen">
-.card{
-  -webkit-box-shadow: 9px 11px 13px 2px rgba(0,0,0,0.54);
--moz-box-shadow: 9px 11px 13px 2px rgba(0,0,0,0.54);
-box-shadow: 9px 11px 13px 2px rgba(0,0,0,0.54);
-}
-
+  .card {
+    -webkit-box-shadow: 9px 11px 13px 2px rgba(0, 0, 0, 0.54);
+    -moz-box-shadow: 9px 11px 13px 2px rgba(0, 0, 0, 0.54);
+    box-shadow: 9px 11px 13px 2px rgba(0, 0, 0, 0.54);
+  }
 </style>
-<div class="input-icons">
-  <div class="card" style="background-color:rgb(60, 179, 113);">
-    <a style="color:white" href="/map"><h2><span class="glyphicon glyphicon-map-marker"></span></h2>
-    <p>Information on waste management</p></a>
+<div class="row col-sm-12">
+  <div class="input-icons col-sm-6">
+    <div class="card col-sm-6" style="background-color:rgb(60, 179, 113);margin-left: 20%;">
+      <a style="color:white;margin-top: 10%;" href="/siteinfo">
+        <h2><span class="glyphicon glyphicon-cog"></span></h2>
+        <p>Information on waste management</p>
+      </a>
+    </div>
   </div>
 
-<div class="input-icons">
-  <div class="card" style="background-color:rgb(207, 41, 91);">
-    <a style="color:white" href="/map"><h2><span class="glyphicon glyphicon-map-marker"></span></h2>
-    <p>Want to find the nearest Bin? Go Here!</p></a>
-  </div>
 
-  <div class="card" style="background-color:rgb(214, 204, 7);">
-    <a style="color:white" href="/waste_classification">
-    <h2><span class="glyphicon glyphicon-trash"></span></h2>
-    <p>Where's that item go? Waste Classification</p></a>
-  </div>
-
-  <div class="card" style="background-color:rgb(153, 29, 2);">
-    <a style="color:white" href="/howtopoints"><h2><span class="glyphicon glyphicon-star"></span></h2>
-    <p><span id="soon">How to earn points?</span></p></a>
-  </div>
-
-  <div class="card" style="background-color:rgb(153, 29, 224);">
-    <a style="color:white" href="/leaderboard"><h2><span class="glyphicon glyphicon-equalizer"></span></h2>
-    <p>Who's toping the charts? Leaderboard</p></a>
-  </div>
-
-  <div class="card" style="background-color:rgb(193, 29, 2);">
-    <a style="color:white" href="/quiz"><span class="material-icons">flaky</span>
-    <p>How well do you know your waste? Quiz Time!</p></a>
+  <div class="input-icons col-sm-6">
+    <div class="card col-sm-6" style="background-color:rgb(207, 41, 91);margin-left: 20%;">
+      <a style="color:white;margin-top: 10%;" href="/map">
+        <h2><span class="glyphicon glyphicon-map-marker"></span></h2>
+        <p>Want to find the nearest Bin? Go Here!</p>
+      </a>
+    </div>
   </div>
 </div>
 
+<div class="row col-sm-12">
+  <div class="input-icons col-sm-6">
+    <div class="card col-sm-6" style="background-color:rgb(214, 204, 7);margin-left: 20%;">
+      <a style="color:white;margin-top: 10%;" href="/waste_classification">
+        <h2><span class="glyphicon glyphicon-trash"></span></h2>
+        <p>Where's that item go? Waste Classification</p>
+      </a>
+    </div>
+  </div>
+
+  <div class="input-icons col-sm-6">
+    <div class="card col-sm-6" style="background-color:rgb(153, 29, 2);margin-left: 20%;">
+      <a style="color:white;margin-top: 10%;" href="/howtopoints">
+        <h2><span class="glyphicon glyphicon-star"></span></h2>
+        <p><span id="soon">How to earn points?</span></p>
+      </a>
+    </div>
+  </div>
+</div>
+
+<div class="row col-sm-12">
+  <div class="input-icons col-sm-6">
+    <div class="card col-sm-6" style="background-color:rgb(153, 29, 224);margin-left: 20%;">
+      <a style="color:white;margin-top: 10%;" href="/leaderboard">
+        <h2><span class="glyphicon glyphicon-equalizer"></span></h2>
+        <p>Who's toping the charts? Leaderboard</p>
+      </a>
+    </div>
+  </div>
+
+  <div class="input-icons col-sm-6">
+    <div class="card col-sm-6" style="background-color:rgb(193, 29, 2);margin-left: 20%;">
+      <a style="color:white;margin-top: 10%;" href="/quiz"><span class="material-icons">flaky</span>
+        <p>How well do you know your waste? Quiz Time!</p>
+      </a>
+    </div>
+  </div>
+</div>
